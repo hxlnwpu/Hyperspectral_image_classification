65e3315a0e78fe418b8722b45b2b677bd0183a59
hxlnwpu <yishengtuwei@foxmail.com>
2019-01-13 21:44:23 +0800

    add CNN  model

--- temp.py
+++ temp.py
@@ -12,7 +12,6 @@ from sklearn.externals import joblib
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import cohen_kappa_score
-import keras
 from keras.layers import Dense, Dropout, Activation,Conv2D,MaxPooling2D,Flatten
 from keras.models import Sequential
 from keras.optimizers import SGD
@@ -85,13 +84,12 @@ def create_patche(dataset, windowSize=5):
     return patches_data   #patches_data为四维数组
 
 # data process
-def data_process(dataset,datalabel,feature_rate=0.15,windowSize=5,test_rate=0.4,method=1):
+def data_process(dataset,datalabel,feature_rate=0.5,windowSize=5,test_rate=0.4,method=1):
     num_class=get_class_num(datalabel)[1]
     if method==0:    #去掉所有0后降到二维再划分训练集和测试集
         new_data_set,new_label_set=get_nozero_data(dataset,datalabel) 
         #pca
-        feature_num=int(new_data_set.shape[1]*feature_rate)
-        pca=PCA(n_components=feature_num)
+        pca=PCA(n_components=int(new_data_set.shape[1]*0.5))  #保留一半的特征
         pca_data=pca.fit_transform(new_data_set)
         #标准化
         scaled_data=preprocessing.scale(pca_data)
@@ -101,8 +99,8 @@ def data_process(dataset,datalabel,feature_rate=0.15,windowSize=5,test_rate=0.4,
         new_data_set=np.reshape(dataset,(-1,dataset.shape[2]))
         new_label_set=np.reshape(datalabel,(-1,1))
         #pca
-        feature_num=int(new_data_set.shape[1]*feature_rate)
-        pca=PCA(n_components=feature_num)
+        #pca=PCA(n_components='mle')
+        pca=PCA(n_components=int(new_data_set.shape[1]*feature_rate))
         pca_data=pca.fit_transform(new_data_set)
         #标准化
         scaled_data=preprocessing.scale(pca_data)
@@ -119,10 +117,10 @@ def data_process(dataset,datalabel,feature_rate=0.15,windowSize=5,test_rate=0.4,
         scaled_data=preprocessing.scale(pca_data)
         new_data_set=np.reshape(scaled_data, (dataset.shape[0],dataset.shape[1],feature_num))
         #创建patch
-        scaled_data=create_patche(new_data_set, windowSize=5)
+        patches_data=create_patche(new_data_set, windowSize=5)
         new_label_set=np.reshape(datalabel,(-1,1))
         #划分数据
-        training_data,test_data,training_label,test_label=train_test_split(scaled_data, new_label_set, test_size=test_rate, random_state=345)
+        training_data,test_data,training_label,test_label=train_test_split(patches_data, new_label_set, test_size=test_rate, random_state=345)
     else:
         print("请选择正确的数据预处理方法，程序即将退出.....")
         return 
@@ -135,131 +133,6 @@ def data_process(dataset,datalabel,feature_rate=0.15,windowSize=5,test_rate=0.4,
            test_label.shape,
            num_class
         )
-    return  scaled_data,new_data_set,new_label_set,training_data,test_data,training_label,test_label,num_class,feature_num
+    return  scaled_data,new_data_set,new_label_set,training_data,test_data,training_label,test_label,num_class
 
-
-    
-# set model
-def set_model(datasetname,scaled_data,new_data_set,
-                new_label_set,training_data,test_data,
-                training_label,test_label,
-                num_class,feature_num,method=0):
-    if method==0:    
-        #设置参数
-        y_train = keras.utils.to_categorical(training_label, num_classes=num_class)
-        model = Sequential()
-        model.add(Dense(64, activation='relu', input_dim=feature_num))
-        model.add(Dropout(0.5))
-        model.add(Dense(64, activation='relu'))
-        model.add(Dropout(0.5))
-        model.add(Dense(num_class, activation='softmax'))
-        sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-        #训练模型
-        model.compile(loss='categorical_crossentropy',optimizer=sgd,metrics=['accuracy'])
-        model.fit(training_data, y_train, epochs=20,batch_size=32)
-        #保存模型
-        model.save(datasetname+"_model.h5")
-        #测试模型 
-        model = load_model(datasetname+"_model.h5")          
-        # score = model.evaluate(test_data, y_test, batch_size=32)
-        # print("正确率为："+str(score[1]))
-        #预测所有数据
-        classes = model.predict(scaled_data, batch_size=32)
-        predict_label=np.zeros((classes.shape[0],1))
-        for i in range(classes.shape[0]):
-            predict_label[i,0]=classes[i,:].argmax()
-    elif method==1:   #SVM
-        clf = svm.SVC(kernel='linear')
-        clf.fit(training_data, training_label)
-        #保存模型
-        joblib.dump(clf, datasetname+"_model.m")
-        #预测所有数据
-        predict_label=joblib.load(datasetname+"_model.m").predict(scaled_data)
-    elif method==2:  #CNN
-        #整理数据
-        training_data = np.reshape(training_data, (training_data.shape[0],training_data.shape[3], training_data.shape[1], training_data.shape[2]))
-        y_train = keras.utils.to_categorical(training_label, num_classes=num_class)
-        #设置参数
-        input_shape= training_data[0].shape
-        C1 = 3*feature_num
-        model = Sequential()
-        model.add(Conv2D(C1, (3, 3), activation='relu', input_shape=input_shape))
-        model.add(Conv2D(3*C1, (3, 3), activation='relu'))
-        model.add(Dropout(0.25))
-        model.add(Flatten())
-        model.add(Dense(6*feature_num, activation='relu'))
-        model.add(Dropout(0.5))
-        model.add(Dense(num_class, activation='softmax'))
-        sgd = SGD(lr=0.0001, decay=1e-6, momentum=0.9, nesterov=True)
-        #训练模型
-        model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-        model.fit(training_data, y_train, batch_size=32, epochs=30)
-        #保存模型
-        model.save(datasetname+"_model.h5")
-        #预测所有数据
-        model = load_model(datasetname+"_model.h5") 
-        scaled_data = np.reshape(scaled_data, (scaled_data.shape[0],scaled_data.shape[3], scaled_data.shape[1], scaled_data.shape[2]))
-        classes = model.predict(scaled_data, batch_size=32)
-        predict_label=np.zeros((classes.shape[0],1))
-        for i in range(classes.shape[0]):
-            predict_label[i,0]=classes[i,:].argmax()
-    else:
-        print("请选择正确的模型，程序即将退出.....")
-        return
-    
-    return predict_label
-
-
-#分类
-def classify(datasetname,data_process_method=1,model_method=1):
-    if datasetname=="Indian_pines_corrected":
-        dataset=Indian_pines_corrected
-        datalabel=Indian_pines_gt
-    elif datasetname=="PaviaU":
-        dataset=PaviaU
-        datalabel=PaviaU_gt
-    elif datasetname=="Pavia":
-        dataset=Pavia
-        datalabel=Pavia_gt
-    else:
-        print("输入参数错误，程序即将退出")
-        return 
-    row=datalabel.shape[0]
-    col=datalabel.shape[1]
-    #数据预处理
-    scaled_data,new_data_set,new_label_set,training_data,test_data,training_label,test_label,num_class,feature_num=data_process(dataset,datalabel,method=data_process_method)
-    #模型预测
-    predict_label=set_model(datasetname,scaled_data,new_data_set,new_label_set,training_data,test_data,training_label,test_label,num_class,feature_num,method=model_method)
-    ##混淆矩阵
-    cm=confusion_matrix(new_label_set, predict_label)
-    print("混淆矩阵如下：")
-    print(cm)
-    ##kappa
-    ck=cohen_kappa_score(new_label_set,predict_label)
-    print("kappa为："+str(ck))
-    ##正确率
-    hit=accuracy_score(new_label_set,predict_label)
-    print("正确率为："+str(hit))
-    #绘图
-    result=np.reshape(predict_label,(row,col))
-    image = Image.fromarray(result)
-    image.save(datasetname+"_predict.tif")
-    print("预测结果已保存为："+datasetname+"_predict.tif")
-    sp.save_rgb(datasetname+"_predict.jpg",result,colors=sp.spy_colors)
-    print("预测效果可查看图片："+datasetname+"_predict.jpg")
-    print("预测结果展示如下：")
-    print(result)
-    return result
-
-if __name__ == '__main__':
-    classify("Indian_pines_corrected",data_process_method=2,model_method=2)
-    #classify("Pavia",data_process_method=1,model_method=0)
-    #classify("PaviaU",data_process_method=1,model_method=0)
-
-
-
-
-
-
-
-
+data_process(Indian_pines_corrected,Indian_pines_gt,feature_rate=0.5,windowSize=5,test_rate=0.4,method=2)
